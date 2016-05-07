a2a4495862603d0ebf113a8af1e2e267706166b4
sgbj <scott.batary@gmail.com>
2016-05-06 23:27:23 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# aspnetcore-angular2-starter
+
+Visual Studio project for ASP.NET Core, Angular 2, and TypeScript
+
+* rc1-update1
+* Visual Studio Update 2
---
+++ src/MyWebApp/Controllers/CharactersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class CharactersController : Controller
+    {
+        [HttpGet]
+        [Produces(typeof(string[]))]
+        public IActionResult Get()
+        {
+            return Json(new[] {
+                "Chewbacca",
+                "Rey",
+                "Finn (FN2187)",
+                "Han Solo",
+                "Leia Organa",
+                "Luke Skywalker",
+                "Poe Dameron",
+                "Kylo Ren",
+                "Supreme Commander Snoke",
+                "R2-D2",
+                "BB8",
+                "C-3PO",
+                "Maz Kanata",
+                "Captain Phasma",
+                "General Hux",
+                "Lor San Tekka",
+            });
+        }
+    }
+}
