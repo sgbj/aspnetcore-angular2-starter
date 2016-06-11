74b05aa3f8a32d70ae37aa0d759f08e57a6d18c7
sgbj <scott.batary@gmail.com>
2016-06-11 14:44:14 -0500

    Modify gitignore

---
+++ src/WebApp/gulpfile.js
@@ -0,0 +1,19 @@
+﻿var gulp = require('gulp');
+
+var paths = {
+    webroot: './wwwroot/',
+    npm: './node_modules/'
+};
+paths.libs = [
+    paths.npm + 'systemjs/dist/**/*',
+    paths.npm + 'angular2/bundles/**/*',
+    paths.npm + 'es6-shim/**/*',
+    paths.npm + 'rxjs/bundles/**/*',
+    paths.npm + 'material-design-lite/dist/**/*'
+];
+paths.libDest = paths.webroot + 'lib/';
+
+gulp.task('default', function () {
+    return gulp.src(paths.libs, { base: paths.npm })
+        .pipe(gulp.dest(paths.libDest));
+});
